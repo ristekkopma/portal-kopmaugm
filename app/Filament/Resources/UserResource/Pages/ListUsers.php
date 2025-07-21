8d7b81cd6b11aecb984e0a16c831f4bedc7ba07c
Azzuro Najma Annaila <azzuronajmaannaila@gmail.com>
2025-07-21 15:16:01 +0700

    feat:Import Penggguna

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -5,15 +5,69 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Action;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Livewire\WithFileUploads;
+use Filament\Notifications\Notification;
+use Maatwebsite\Excel\Facades\Excel;
+
 
 class ListUsers extends ListRecords
 {
+
+    use WithFileUploads;
+
     protected static string $resource = UserResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
-        ];
+
+            Action::make('import')
+            ->label('Import')
+            ->icon('heroicon-m-arrow-up-tray')
+
+            ->modalHeading('Import Data Pengguna')
+            
+            ->form([
+                FileUpload::make('file')
+                    ->label('Upload CSV')
+                    ->acceptedFileTypes(['text/csv'])
+                    ->required()
+                    ->disk('local')
+                    ->storeFiles(false),
+            ])
+            ->action(function (array $data): void {
+                try {
+                    $import = new UserImport;
+                    Excel::import($import, $data['file']);
+
+                    $notif = Notification::make()
+                        ->title('Berhasil mengimpor data pengguna.');
+
+                    if ($import->hasDuplicates) {
+                        $notif->body('Beberapa data diabaikan karena duplikat.');
+                    }
+
+                    $notif->success()->send();
+
+                } catch (\Exception $e) {
+                    Notification::make()
+                        ->title('Gagal mengimpor')
+                        ->body($e->getMessage())
+                        ->danger()
+                        ->send();
+                }
+            })
+
+            
+            
+            ->modalSubmitActionLabel('Import')
+            // ->modalWidth('md')
+            ->color('primary')
+    ];
     }
 }
