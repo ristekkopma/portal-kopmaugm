1887d4fe06dc2f3aa62dd5239e985eff1834871c
Azzuro Najma Annaila <azzuronajmaannaila@gmail.com>
2025-07-27 13:29:18 +0700

    7

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LandingController;
 
-Route::get('/', [LandingController::class, 'index']);
+Route::get('/', function () {
+    return view('landing');
+});
 
 
 
